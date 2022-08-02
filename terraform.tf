7ed24daf0d045318ce32294b9cc0357645ce7ec8
Bruce Becker <brucellino@protonmail.com>
2022-08-02 19:09:31 +0200

    chore: add required providers
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- terraform.tf
+++ terraform.tf
@@ -1,15 +1,9 @@
-# Use this file to declare the terraform configuration
-# Add things like:
-# - required version
-# - required providers
-# Do not add things like:
-# - provider configuration
-# - backend configuration
-# These will be declared in the terraform document which consumes the module.
-
 terraform {
   required_version = ">1.2.0"
   required_providers {
-    # Add your required providers here.
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.21.0"
+    }
   }
 }
